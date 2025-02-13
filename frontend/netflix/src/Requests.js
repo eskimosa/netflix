32b72f709af253c6867e59929fbfb4bc3cf92eb2
eskimosa <esskimosa@gmail.com>
2025-02-13 20:48:15 +0100

    worked on fetching and rendering different movie categories using tmdb apis

---
+++ frontend/netflix/src/Requests.js
@@ -0,0 +1,11 @@
+const key = '';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=27&language=en-US&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=2`,
+};
+
+export default requests;
--- frontend/netflix/src/pages/Home.jsx
+++ frontend/netflix/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import baseUrl from '../components/shared/baseUrl'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
         <Main />
+        {/* <Row title='Top Rated' fetchURL={`${baseUrl}/api/movies/`} /> */}
+        <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+        <Row title='UpComing' fetchURL={requests.requestUpcoming} />
+        <Row title='Popular' fetchURL={requests.requestPopular} />
+        <Row title='Trending' fetchURL={requests.requestTrending} />
+        <Row title='Horror' fetchURL={requests.requestHorror} />
     </>
   )
 }
